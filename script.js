abad1eb13af764fc36b4459bfe7cfa17cd002bb0
felippejfdev <felippedev99@gmail.com>
2024-08-13 10:25:54 -0300

    commite

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+
+const myObserver= new IntersectionObserver((entries)=>{
+
+    entries.forEach((entry)=>{
+        if(entry.isIntersecting){
+            entry.target.classList.add("show")
+        }
+        else{
+            entry.target.classList.remove("show")
+        }
+    })
+    
+    } )
+    
+    const elements= document.querySelectorAll(".conteiner-1")
+    
+    
+    elements.forEach((element)=> myObserver.observe(element))
